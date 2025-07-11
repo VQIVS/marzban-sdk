16a89853894289f80bee487633eb91ecaa26ca42
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2025-07-11 20:36:29 +0330

    --wip-- [skip ci]

--- handlers/user.go
+++ handlers/user.go
@@ -212,3 +212,218 @@ func (mc *MarzbanClient) GetUserInbounds(username string) ([]string, error) {
 
 	return response.Inbounds, nil
 }
+
+func (mc *MarzbanClient) GetUserProxies(username string) ([]string, error) {
+	endpoint := client.GetUserByUsernameEndpoint(username)
+	fullURL, err := utils.StringToURL(mc.Client.BaseURL + endpoint)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := mc.Client.HttpClient.Get(fullURL.String())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, &models.ErrorResponse{
+			Message: "HTTP " + resp.Status,
+			Detail:  "Failed to get user proxies, status code: " + resp.Status + ", body: " + string(responseBody),
+		}
+	}
+
+	var response struct {
+		Proxies []string `json:"proxies"`
+	}
+	if err := json.Unmarshal(responseBody, &response); err != nil {
+		return nil, err
+	}
+
+	return response.Proxies, nil
+}
+func (mc *MarzbanClient) GetUserUsage(username string) (int64, error) {
+	endpoint := client.GetUserByUsernameEndpoint(username)
+	fullURL, err := utils.StringToURL(mc.Client.BaseURL + endpoint)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := mc.Client.HttpClient.Get(fullURL.String())
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return 0, &models.ErrorResponse{
+			Message: "HTTP " + resp.Status,
+			Detail:  "Failed to get user usage, status code: " + resp.Status + ", body: " + string(responseBody),
+		}
+	}
+
+	var response struct {
+		Usage int64 `json:"usage"`
+	}
+	if err := json.Unmarshal(responseBody, &response); err != nil {
+		return 0, err
+	}
+
+	return response.Usage, nil
+}
+func (mc *MarzbanClient) GetUserStatus(username string) (string, error) {
+	endpoint := client.GetUserByUsernameEndpoint(username)
+	fullURL, err := utils.StringToURL(mc.Client.BaseURL + endpoint)
+	if err != nil {
+		return "", err
+	}
+	resp, err := mc.Client.HttpClient.Get(fullURL.String())
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", &models.ErrorResponse{
+			Message: "HTTP " + resp.Status,
+			Detail:  "Failed to get user status, status code: " + resp.Status + ", body: " + string(responseBody),
+		}
+	}
+
+	var response struct {
+		Status string `json:"status"`
+	}
+	if err := json.Unmarshal(responseBody, &response); err != nil {
+		return "", err
+	}
+
+	return response.Status, nil
+}
+
+func (mc *MarzbanClient) GetUserExpire(username string) (int64, error) {
+	endpoint := client.GetUserByUsernameEndpoint(username)
+	fullURL, err := utils.StringToURL(mc.Client.BaseURL + endpoint)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := mc.Client.HttpClient.Get(fullURL.String())
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return 0, &models.ErrorResponse{
+			Message: "HTTP " + resp.Status,
+			Detail:  "Failed to get user expire time, status code: " + resp.Status + ", body: " + string(responseBody),
+		}
+	}
+
+	var response struct {
+		Expire int64 `json:"expire"`
+	}
+	if err := json.Unmarshal(responseBody, &response); err != nil {
+		return 0, err
+	}
+
+	return response.Expire, nil
+}
+
+func (mc *MarzbanClient) ResetUserUsage(username string) error {
+	endpoint := client.GetUserByUsernameEndpoint(username) + client.EndpointUserReset
+
+	fullURL, err := utils.StringToURL(mc.Client.BaseURL + endpoint)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, fullURL.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := mc.Client.HttpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		responseBody, _ := io.ReadAll(resp.Body)
+		return &models.ErrorResponse{
+			Message: "HTTP " + resp.Status,
+			Detail:  "Failed to reset user usage, status code: " + resp.Status + ", body: " + string(responseBody),
+		}
+	}
+	return nil
+}
+
+//TODO: fix user sub revokes
+
+func (mc *MarzbanClient) RevokeUserSub(username string) error {
+	endpoint := client.GetUserByUsernameEndpoint(username) + client.EndpointUserRevokeSubsription
+
+	fullURL, err := utils.StringToURL(mc.Client.BaseURL + endpoint)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, fullURL.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := mc.Client.HttpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		responseBody, _ := io.ReadAll(resp.Body)
+		return &models.ErrorResponse{
+			Message: "HTTP " + resp.Status,
+			Detail:  "Failed to revoke user subscription, status code: " + resp.Status + ", body: " + string(responseBody),
+		}
+	}
+	return nil
+}
+
+func (mc *MarzbanClient) GetExpiredUsers() ([]models.User, error) {
+	fullURL, err := utils.StringToURL(mc.Client.BaseURL + client.EndpointUsersExpired)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := mc.Client.HttpClient.Get(fullURL.String())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, &models.ErrorResponse{
+			Message: "HTTP " + resp.Status,
+			Detail:  "Failed to get expired users, status code: " + resp.Status + ", body: " + string(responseBody),
+		}
+	}
+
+	var users []models.User
+	if err := json.Unmarshal(responseBody, &users); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
